ef4f594b66c78254e8d04637070fc88b23dfbf33
Azeem <azeemahmed101@gmail.com>
2020-06-05 16:49:04 +0200

    create article create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,22 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
+  include ArticlesHelper
   def index
     @articles = Article.all
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article) # This sends you to show action, you can also use redirect_to @article
   end
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 module ArticlesHelper
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
