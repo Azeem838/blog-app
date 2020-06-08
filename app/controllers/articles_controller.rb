eba1cf79e3b012e9f8d43b3728f2e2071de85b88
rajkumardas2701 <rajkumardas2701@gmail.com>
2020-06-08 20:25:21 +0530

    adding comment section on article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,9 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    @comment = Comment.new
+    # @comment = @article.comments.new
+    @comment.article_id = @article.id    
   end
 
   def new
@@ -17,8 +20,10 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
-    
-    flash.notice = "Article '#{@article.title}' has been created"
+
+    # flash.notice = "Article '#{@article.title}' has been created"
+    flash[:notice] = 'Article was created successfully.'
+
 
     redirect_to article_path(@article) # This sends you to show action, you can also use redirect_to @article
   end
@@ -26,6 +31,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
+    flash.notice = "Article '#{@article.title}' has been deleted"
     redirect_to articles_path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :articles
+  root to: 'articles#index'
+  resources :articles do
+    resources :comments
+  end
 end
